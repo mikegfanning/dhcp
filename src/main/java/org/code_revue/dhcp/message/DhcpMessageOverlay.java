5ad33eff376cd48e4e51bea5419d77b96c4c4172
Mike Fanning <mikegfanning@gmail.com>
2014-07-29 21:11:42 -0700

    Added setters for all the header fields.

--- src/main/java/org/code_revue/dhcp/message/DhcpMessageOverlay.java
+++ src/main/java/org/code_revue/dhcp/message/DhcpMessageOverlay.java
@@ -35,56 +35,125 @@ public class DhcpMessageOverlay {
         return DhcpOpCode.getByNumericCode(headerData.get(0));
     }
 
+    public void setOpCode(DhcpOpCode code) {
+        headerData.put(0, (byte) code.getNumericCode());
+    }
+
     public HardwareType getHardwareType() {
         return HardwareType.getByNumericCode(headerData.get(1));
+    }
+
+    public void setHardwareType(HardwareType type) {
+        headerData.put(1, (byte) type.getNumericCode());
     }
 
     public byte getHardwareAddressLength() {
         return headerData.get(2);
     }
 
+    public void setHardwareAddressLength(byte length) {
+        headerData.put(2, length);
+    }
+
     public byte getHops() {
         return headerData.get(3);
+    }
+
+    public void setHops(byte hops) {
+        headerData.put(3, hops);
     }
 
     public int getTransactionId() {
         return headerData.getInt(4);
     }
 
+    public void setTransactionId(int id) {
+        headerData.putInt(4, id);
+    }
+
     public short getSeconds() {
         return headerData.getShort(8);
+    }
+
+    public void setSeconds(short seconds) {
+        headerData.putShort(8, seconds);
     }
 
     public boolean isBroadcast() {
         return (headerData.get(10) & 0b10000000) != 0;
     }
 
+    public void setBroadcast(boolean broadcast) {
+        byte broadcastByte = headerData.get(10);
+        if (broadcast) {
+            broadcastByte = (byte) (broadcastByte | 0b10000000);
+        } else {
+            broadcastByte = (byte) (broadcastByte & 0b01111111);
+        }
+        headerData.put(10, broadcastByte);
+    }
+
     public byte[] getClientIpAddress() {
         return getByteArray(12, 4);
+    }
+
+    public void setClientIpAddress(byte[] ipAddress) {
+        headerData.position(12);
+        headerData.put(ipAddress, 0, Math.min(ipAddress.length, 4));
     }
 
     public byte[] getYourIpAddress() {
         return getByteArray(16, 4);
     }
 
+    public void setYourIpAddress(byte[] ipAddress) {
+        headerData.position(16);
+        headerData.put(ipAddress, 0, Math.min(ipAddress.length, 4));
+    }
+
     public byte[] getServerIpAddress() {
         return getByteArray(20, 4);
+    }
+
+    public void setServerIpAddress(byte[] ipAddress) {
+        headerData.position(20);
+        headerData.put(ipAddress, 0, Math.min(ipAddress.length, 4));
     }
 
     public byte[] getGatewayIpAddress() {
         return getByteArray(24, 4);
     }
 
+    public void setGatewayIpAddress(byte[] ipAddress) {
+        headerData.position(24);
+        headerData.put(ipAddress, 0, Math.min(ipAddress.length, 4));
+    }
+
     public byte[] getClientHardwareAddress() {
         return getByteArray(28, 16);
+    }
+
+    public void setClientHardwareAddress(byte[] hardwareAddress) {
+        headerData.position(28);
+        headerData.put(hardwareAddress, 0, Math.min(hardwareAddress.length, 16));
     }
 
     public String getServerName() {
         return new String(getByteArray(44, 64));
     }
 
+    public void setServerName(byte[] serverName) {
+        headerData.position(44);
+        headerData.put(serverName, 0, Math.min(serverName.length, 64));
+    }
+
     public String getBootFilename() {
         return new String(getByteArray(108, 128));
+    }
+
+    public void setBootFileName(byte[] fileName) {
+        headerData.position(108);
+        headerData.put(fileName, 0, Math.min(fileName.length, 128));
     }
 
     private byte[] getByteArray(int offset, int length) {
